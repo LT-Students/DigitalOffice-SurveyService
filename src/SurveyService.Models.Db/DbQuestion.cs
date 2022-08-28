2c4ef106d3cd64fdf4f247006387b9b717eff4a2
maybee16 <mayaochir@gmail.com>
2022-08-28 19:11:08 +0900

    Changed parameter names, added empty line, removed the curly braces after namespaces

--- src/SurveyService.Models.Db/DbQuestion.cs
+++ src/SurveyService.Models.Db/DbQuestion.cs
@@ -3,58 +3,58 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 
-namespace LT.DigitalOffice.SurveyService.Models.Db
+namespace LT.DigitalOffice.SurveyService.Models.Db;
+
+public class DbQuestion
 {
-  public class DbQuestion
+  public const string TableName = "Questions";
+
+  public Guid Id { get; set; }
+  public Guid? GroupId { get; set; }
+  public string Content { get; set; }
+  public DateTime? Deadline { get; set; }
+  public bool HasRealTimeResult { get; set; }
+  public bool IsAnonymous { get; set; }
+  public bool IsRevoteAvailable { get; set; }
+  public bool IsObligatory { get; set; }
+  public bool IsPrivate { get; set; }
+  public bool HasMultipleChoice { get; set; }
+  public bool HasCustomOptions { get; set; }
+  public Guid CreatedBy { get; set; }
+  public DateTime CreatedAtUTC { get; set; }
+  public Guid? ModifiedBy { get; set; }
+  public DateTime? ModifiedAtUTC { get; set; }
+  public bool IsActive { get; set; }
+
+  public ICollection<DbOption> Options { get; set; }
+  public DbGroup Group { get; set; }
+
+  public DbQuestion()
   {
-    public const string TableName = "Questions";
-
-    public Guid Id { get; set; }
-    public Guid? GroupId { get; set; }
-    public string Content { get; set; }
-    public DateTime? Deadline { get; set; }
-    public bool HasRealTimeResult { get; set; }
-    public bool IsAnonymous { get; set; }
-    public bool IsRevoteAvailable { get; set; }
-    public bool IsObligatory { get; set; }
-    public bool IsPrivate { get; set; }
-    public bool HasMultipleChoice { get; set; }
-    public bool HasCustomOptions { get; set; }
-    public Guid CreatedBy { get; set; }
-    public DateTime CreatedAtUTC { get; set; }
-    public Guid? ModifiedBy { get; set; }
-    public DateTime? ModifiedAtUTC { get; set; }
-    public bool IsActive { get; set; }
-    public ICollection<DbOption> Options { get; set; }
-    public DbGroup Group { get; set; }
-
-    public DbQuestion()
-    {
-      Options = new HashSet<DbOption>();
-    }
+    Options = new HashSet<DbOption>();
   }
+}
 
-  public class QuestionConfiguration : IEntityTypeConfiguration<DbQuestion>
+public class QuestionConfiguration : IEntityTypeConfiguration<DbQuestion>
+{
+  public void Configure(EntityTypeBuilder<DbQuestion> builder)
   {
-    public void Configure(EntityTypeBuilder<DbQuestion> builder)
-    {
-      builder
-          .ToTable(DbQuestion.TableName);
+    builder
+        .ToTable(DbQuestion.TableName);
 
-      builder
-          .HasKey(q => q.Id);
+    builder
+        .HasKey(q => q.Id);
 
-      builder
-          .Property(q => q.Content)
-          .IsRequired();
+    builder
+        .Property(q => q.Content)
+        .IsRequired();
 
-      builder
-          .HasMany(o => o.Options)
-          .WithOne(o => o.Questions);
+    builder
+        .HasMany(q => q.Options)
+        .WithOne(o => o.Questions);
 
-      builder
-          .HasOne(g => g.Group)
-          .WithMany(o => o.Questions);
-    }
+    builder
+        .HasOne(q => q.Group)
+        .WithMany(g => g.Questions);
   }
 }
