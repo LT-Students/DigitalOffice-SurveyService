a39f705e2acde4961d088d672ab3f55ad5588b99
Maya O <55130636+maybee16@users.noreply.github.com>
2022-08-29 17:35:57 +0900

    Created DB migration and model for Questions (#2)
    * Created DB migration and model for Questions http://outsourcing.nat.tepkom.ru:20080/redmine/issues/1128
    
    * Changed "ModifiedBy" property to nullable
    
    * Changed parameter names, added empty line, removed the curly braces after namespaces
    
    * Changed property name
    
    * Fixed migration name, column names and property names in DbQuestion
    
    * Removed extra spaces

---
+++ src/SurveyService.Data.Provider.MsSql.Ef/Migrations/20220826180800_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using LT.DigitalOffice.SurveyService.Models.Db;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace LT.DigitalOffice.SurveyService.Data.Provider.MsSql.Ef.Migrations;
+
+[DbContext(typeof(SurveyServiceDbContext))]
+[Migration("20220826180800_InitialCreate")]
+class InitialCreate : Migration
+{
+  protected override void Up(MigrationBuilder builder)
+  {
+    builder.CreateTable(
+      name: DbQuestion.TableName,
+      columns: table => new
+      {
+        Id = table.Column<Guid>(nullable: false),
+        GroupId = table.Column<Guid>(nullable: true),
+        Content = table.Column<string>(nullable: false),
+        Deadline = table.Column<DateTime>(nullable: true),
+        HasRealTimeResult = table.Column<bool>(nullable: false),
+        IsAnonymous = table.Column<bool>(nullable: false),
+        IsRevoteAvailable = table.Column<bool>(nullable: false),
+        IsObligatory = table.Column<bool>(nullable: false),
+        IsPrivate = table.Column<bool>(nullable: false),
+        HasMultipleChoice = table.Column<bool>(nullable: false),
+        HasCustomOptions = table.Column<bool>(nullable: false),
+        CreatedBy = table.Column<Guid>(nullable: false),
+        CreatedAtUtc = table.Column<DateTime>(nullable: false),
+        ModifiedBy = table.Column<Guid>(nullable: true),
+        ModifiedAtUtc = table.Column<DateTime>(nullable: true),
+        IsActive = table.Column<bool>(nullable: false)
+      },
+      constraints: table =>
+      {
+        table.PrimaryKey($"PK_{DbQuestion.TableName}", x => x.Id);
+      });
+  }
+}
---
+++ src/SurveyService.Models.Db/DbQuestion.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+
+namespace LT.DigitalOffice.SurveyService.Models.Db;
+
+public class DbQuestion
+{
+  public const string TableName = "Questions";
+
+  public Guid Id { get; set; }
+  public Guid? GroupId { get; set; }
+  public string Content { get; set; }
+  public DateTime? Deadline { get; set; }
+  public bool HasRealTimeResult { get; set; }
+  public bool IsAnonymous { get; set; }
+  public bool IsRevoteAvailable { get; set; }
+  public bool IsObligatory { get; set; }
+  public bool IsPrivate { get; set; }
+  public bool HasMultipleChoice { get; set; }
+  public bool HasCustomOptions { get; set; }
+  public bool IsActive { get; set; }
+  public Guid CreatedBy { get; set; }
+  public DateTime CreatedAtUtc { get; set; }
+  public Guid? ModifiedBy { get; set; }
+  public DateTime? ModifiedAtUtc { get; set; }
+
+  public ICollection<DbOption> Options { get; set; }
+  public DbGroup Group { get; set; }
+
+  public DbQuestion()
+  {
+    Options = new HashSet<DbOption>();
+  }
+}
+
+public class QuestionConfiguration : IEntityTypeConfiguration<DbQuestion>
+{
+  public void Configure(EntityTypeBuilder<DbQuestion> builder)
+  {
+    builder
+      .ToTable(DbQuestion.TableName);
+
+    builder
+      .HasKey(q => q.Id);
+
+    builder
+      .Property(q => q.Content)
+      .IsRequired();
+
+    builder
+      .HasMany(q => q.Options)
+      .WithOne(o => o.Question);
+
+    builder
+      .HasOne(q => q.Group)
+      .WithMany(g => g.Questions);
+  }
+}
