68b9b52892c196bda0d64516e9413705ce17e864
Levliv <50172336+Levliv@users.noreply.github.com>
2022-09-01 14:56:10 +0300

    Added HttpStatusCode to CreateGroupCommand.cs

--- src/SurveyService.Business/Commands/Group/CreateGroupCommand.cs
+++ src/SurveyService.Business/Commands/Group/CreateGroupCommand.cs
@@ -6,6 +6,7 @@ using LT.DigitalOffice.SurveyService.Data.Interfaces;
 using LT.DigitalOffice.SurveyService.Mappers.Db.Interfaces;
 using LT.DigitalOffice.SurveyService.Models.Dto.Requests.Group;
 using LT.DigitalOffice.SurveyService.Validation.Group.Interfaces;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,17 +21,20 @@ public class CreateGroupCommand : ICreateGroupCommand
   private readonly IDbGroupMapper _dbGroupMapper;
   private readonly IResponseCreator _responseCreator;
   private readonly ICreateGroupRequestValidator _validator;
+  private readonly IHttpContextAccessor _httpContextAccessor;
   
   public CreateGroupCommand(
     IGroupRepository groupRepository,
     IDbGroupMapper dbGroupMapper,
     IResponseCreator responseCreator,
-    ICreateGroupRequestValidator validator)
+    ICreateGroupRequestValidator validator,
+    IHttpContextAccessor httpContextAccessor)
   {
     _groupRepository = groupRepository;
     _dbGroupMapper = dbGroupMapper;
     _responseCreator = responseCreator;
     _validator = validator;
+    _httpContextAccessor = httpContextAccessor;
   }
 
   public async Task<OperationResultResponse<Guid?>> ExecuteAsync(CreateGroupRequest request)
@@ -53,6 +57,8 @@ public class CreateGroupCommand : ICreateGroupCommand
         HttpStatusCode.BadRequest,
         new List<string> { "Error occured while creating group." });
     }
+    
+    _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.Created;
     
     return new OperationResultResponse<Guid?>(body: createGroupGuid);
   }
