bc129c6a839a0da190cf046bc8b6ee950f9b1562
Levliv <50172336+Levliv@users.noreply.github.com>
2022-09-04 19:18:10 +0300

    Fixed DI in IDbGroupMapper.cs, IDbQuestionMapper.cs, IGroupRepository.cs

--- src/SurveyService.Mappers/Db/Interfaces/IDbGroupMapper.cs
+++ src/SurveyService.Mappers/Db/Interfaces/IDbGroupMapper.cs
@@ -1,8 +1,10 @@
+using LT.DigitalOffice.Kernel.Attributes;
 using LT.DigitalOffice.SurveyService.Models.Db;
 using LT.DigitalOffice.SurveyService.Models.Dto.Requests.Group;
 
 namespace LT.DigitalOffice.SurveyService.Mappers.Db.Interfaces;
 
+[AutoInject]
 public interface IDbGroupMapper
 {
   DbGroup Map(CreateGroupRequest createGroupRequest);
--- src/SurveyService.Mappers/Db/Interfaces/IDbQuestionMapper.cs
+++ src/SurveyService.Mappers/Db/Interfaces/IDbQuestionMapper.cs
@@ -1,9 +1,11 @@
+using LT.DigitalOffice.Kernel.Attributes;
 using LT.DigitalOffice.SurveyService.Models.Db;
 using LT.DigitalOffice.SurveyService.Models.Dto.Requests.Question;
 using System;
 
 namespace LT.DigitalOffice.SurveyService.Mappers.Db.Interfaces;
 
+[AutoInject]
 public interface IDbQuestionMapper
 {
   DbQuestion Map(CreateQuestionRequest createQuestionRequest, Guid? groupId);
