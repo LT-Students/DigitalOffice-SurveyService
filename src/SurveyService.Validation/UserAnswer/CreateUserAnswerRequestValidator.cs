76a711301945b4c5d860e35f7ba37d4d3912df0a
vasis <vasily.condratiev@yandex.ru>
2022-09-06 01:45:59 +0300

    Refactored validator

--- src/SurveyService.Validation/UserAnswer/CreateUserAnswerRequestValidator.cs
+++ src/SurveyService.Validation/UserAnswer/CreateUserAnswerRequestValidator.cs
@@ -7,13 +7,9 @@ namespace LT.DigitalOffice.SurveyService.Validation.UserAnswer;
 
 public class CreateUserAnswerRequestValidator : AbstractValidator<CreateUserAnswerRequest>, ICreateUserAnswerRequestValidator
 {
-  private readonly IOptionRepository _optionRepository;
-
   public CreateUserAnswerRequestValidator(
-    IOptionRepository optionRepository)
+    IOptionRepository _optionRepository)
   {
-    _optionRepository = optionRepository;
-
     RuleFor(request => request.OptionId)
       .MustAsync(async (optionId, _) => await _optionRepository.DoesExistAsync(optionId))
       .WithMessage("The option id doesn't exist.");
