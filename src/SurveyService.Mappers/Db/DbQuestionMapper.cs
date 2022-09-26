6590e8c5a8af874286ad9560eb2150da4ad034ad
Lev <50172336+Levliv@users.noreply.github.com>
2022-09-26 14:56:27 +0300

    Fixed circular dependency injection in DbQuestionMapper.cs (#10)
    * Fixed circular dependency injection in DbQuestionMapper.cs
    
    * style

--- src/SurveyService.Mappers/Db/DbQuestionMapper.cs
+++ src/SurveyService.Mappers/Db/DbQuestionMapper.cs
@@ -12,16 +12,13 @@ public class DbQuestionMapper : IDbQuestionMapper
 {
   private readonly IHttpContextAccessor _httpContextAccessor;
   private readonly IDbOptionMapper _dbOptionMapper;
-  private readonly IDbQuestionMapper _dbQuestionMapper;
 
   public DbQuestionMapper(
     IHttpContextAccessor httpContextAccessor, 
-    IDbOptionMapper dbOptionMapper,
-    IDbQuestionMapper dbSingleQuestionMapper)
+    IDbOptionMapper dbOptionMapper)
   {
     _httpContextAccessor = httpContextAccessor;
     _dbOptionMapper = dbOptionMapper;
-    _dbQuestionMapper = dbSingleQuestionMapper;
   }
 
   public DbQuestion Map(CreateSingleQuestionRequest request)
@@ -29,32 +26,32 @@ public class DbQuestionMapper : IDbQuestionMapper
     Guid questionId = Guid.NewGuid();
 
     return request is null
-    ? null
-    : new DbQuestion
-    {
-      Id = questionId,
-      GroupId = request.GroupId,
-      Content = request.Content,
-      Deadline = request.Deadline,
-      HasRealTimeResult = request.HasRealTimeResult,
-      IsAnonymous = request.IsAnonymous,
-      IsRevoteAvailable = request.IsRevoteAvailable,
-      IsObligatory = request.IsObligatory,
-      IsPrivate = request.IsPrivate,
-      HasMultipleChoice = request.HasMultipleChoice,
-      HasCustomOptions = request.HasCustomOptions,
-      IsActive = true,
-      CreatedBy = _httpContextAccessor.HttpContext.GetUserId(),
-      CreatedAtUtc = DateTime.UtcNow,
-      Options = request.Options?.Select(option => _dbOptionMapper.Map(option, questionId)).ToList()
-    };
+      ? null
+      : new DbQuestion
+      {
+        Id = questionId,
+        GroupId = request.GroupId,
+        Content = request.Content,
+        Deadline = request.Deadline,
+        HasRealTimeResult = request.HasRealTimeResult,
+        IsAnonymous = request.IsAnonymous,
+        IsRevoteAvailable = request.IsRevoteAvailable,
+        IsObligatory = request.IsObligatory,
+        IsPrivate = request.IsPrivate,
+        HasMultipleChoice = request.HasMultipleChoice,
+        HasCustomOptions = request.HasCustomOptions,
+        IsActive = true,
+        CreatedBy = _httpContextAccessor.HttpContext.GetUserId(),
+        CreatedAtUtc = DateTime.UtcNow,
+        Options = request.Options?.Select(option => _dbOptionMapper.Map(option, questionId)).ToList()
+      };
   }
   
   public DbQuestion Map(CreateGroupQuestionRequest request, Guid groupId, DateTime? groupDeadline, bool groupHasRealTimeResult)
   {
     return request is null
       ? null
-      : _dbQuestionMapper.Map(
+      : Map(
         new CreateSingleQuestionRequest 
         {
           GroupId = groupId,
