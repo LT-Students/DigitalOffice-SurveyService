2ae97e9bbcf86435349e8bf72bb3978d53af3f76
Levliv <50172336+Levliv@users.noreply.github.com>
2022-10-02 21:05:06 +0300

    Fixed AutoInject DI

--- src/SurveyService.Mappers/Responses/Interfaces/IQuestionResponseMapper.cs
+++ src/SurveyService.Mappers/Responses/Interfaces/IQuestionResponseMapper.cs
@@ -1,8 +1,10 @@
+using LT.DigitalOffice.Kernel.Attributes;
 using LT.DigitalOffice.SurveyService.Models.Db;
 using LT.DigitalOffice.SurveyService.Models.Dto.Responses.Question;
 
 namespace LT.DigitalOffice.SurveyService.Mappers.Responses.Interfaces;
 
+[AutoInject]
 public interface IQuestionResponseMapper
 {
   QuestionResponse Map(DbQuestion dbQuestion);
